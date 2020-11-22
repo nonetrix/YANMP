e45fce73ebcae64fa4843a6a2321a8e436c22d6c
nonetrix <45698918+nonetrix@users.noreply.github.com>
2020-11-21 20:12:09 -0600

    clean up code I made at 3AM
    what the fuck was I thinking

--- main.py
+++ main.py
@@ -14,18 +14,15 @@ class PlayerThread(Thread):
         if SongSelected.endswith('.mp3'):  
             song = AudioSegment.from_mp3(SongSelected)
             play(song)
-            exit(0)
         if SongSelected.endswith('.wav'):  
             song = AudioSegment.from_wav(SongSelected)
             play(song)
-            exit(0)
         if SongSelected.endswith('.ogg'):  
             song = AudioSegment.from_ogg(SongSelected)
             play(song)
-            exit(0)
         else:
             print("error audio file unsupported try mp3 wav ogg")
-            exit(0)
+            
 
 class PrinterTread(Thread):
     def run(self):
@@ -38,13 +35,9 @@ t2 = PrinterTread(args=("SongSelected"))
 
 while True:
     if t.is_alive() is False:
-        print(SongSelected)
-    while True:
-        SongSelected = MusicFolder + "/" + random.choice(os.listdir(MusicFolder))
-        t.start()
-        if t2.is_alive() is False:
-            t2.start()
-        
-        t.join()
-        
         t = PlayerThread(args=("SongSelected"))
+        t.start()
+        SongSelected = MusicFolder + "/" + random.choice(os.listdir(MusicFolder))
+    if t2.is_alive() is False:
+        t2 = PrinterTread(args=("SongSelected"))
+        t2.start()

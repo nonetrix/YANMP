a4e1fa5d84664a3277244e428343bcfaa0c8bf31
nonetrix <45698918+nonetrix@users.noreply.github.com>
2020-11-22 21:22:13 -0600

    and I oop
    forgot to remove some stuffs

--- main.py
+++ main.py
@@ -4,10 +4,7 @@ from tinytag import TinyTag
 #MusicFolder = "./testfolder"
 MusicFolder = "/home/nonetrix/games/music"
 
-SongCompleted = 0
-
 while True:
-    if SongCompleted == 0:
         SongSelected = MusicFolder + "/" + random.choice(os.listdir(MusicFolder))
         tag = TinyTag.get(SongSelected)
         pygame.mixer.init(tag.samplerate)

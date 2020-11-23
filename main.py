1861ea0ea851170942f5d73c8031c25caf52df95
nonetrix <45698918+nonetrix@users.noreply.github.com>
2020-11-22 21:19:05 -0600

    clean up code for pygame
    removes threading since it's no longer needed with pygame

--- main.py
+++ main.py
@@ -1,33 +1,17 @@
-import random, time, os, threading, pygame
+import random, time, os, pygame
 from tinytag import TinyTag
-from threading import Thread
 
 #MusicFolder = "./testfolder"
 MusicFolder = "/home/nonetrix/games/music"
 
-class PlayerThread(Thread):
-    def run(self):
+SongCompleted = 0
+
+while True:
+    if SongCompleted == 0:
+        SongSelected = MusicFolder + "/" + random.choice(os.listdir(MusicFolder))
         tag = TinyTag.get(SongSelected)
         pygame.mixer.init(tag.samplerate)
         pygame.mixer.music.load(SongSelected)
         pygame.mixer.music.play()
+        print("now playing", SongSelected)
         time.sleep(tag.duration)
-
-
-class PrinterTread(Thread):
-    def run(self):
-        while True:
-            print("now playing", SongSelected)
-            time.sleep(1)
-
-t = PlayerThread(args=("SongSelected"))
-t2 = PrinterTread(args=("SongSelected"))
-
-while True:
-    if t.is_alive() is False:
-        SongSelected = MusicFolder + "/" + random.choice(os.listdir(MusicFolder))
-        t = PlayerThread(args=("SongSelected"))
-        t.start()
-    if t2.is_alive() is False:
-        t2 = PrinterTread(args=("SongSelected"))
-        t2.start()

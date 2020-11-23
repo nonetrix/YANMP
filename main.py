42cd704e8548ba6e562ea42a9e9a6512371b3da2
nonetrix <45698918+nonetrix@users.noreply.github.com>
2020-11-23 00:06:54 -0600

    add very basic UI

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-import random, time, os, pygame
+import pygame.mixer, os, time, threading, curses, random
 from tinytag import TinyTag
 
 #MusicFolder = "./testfolder"
 MusicFolder = "/home/nonetrix/games/music"
+
+stdscr = curses.initscr()
+stdscr.clear()
 
 while True:
         SongSelected = MusicFolder + "/" + random.choice(os.listdir(MusicFolder))
         tag = TinyTag.get(SongSelected)
         pygame.mixer.init(tag.samplerate)
         pygame.mixer.music.load(SongSelected)
+        stdscr.addstr(SongSelected)
+        stdscr.refresh()
         pygame.mixer.music.play()
-        print("now playing", SongSelected)
         time.sleep(tag.duration)
+        stdscr.clear()

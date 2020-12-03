833d1e094e95f6033e09a70f8285104105b180ee
nonetrix <45698918+nonetrix@users.noreply.github.com>
2020-12-02 19:42:37 -0600

    pause and play and skip

--- main.py
+++ main.py
@@ -4,17 +4,39 @@ from tinytag import TinyTag
 #MusicFolder = "./testfolder"
 MusicFolder = "/home/nonetrix/games/music"
 
+pause = 0
+
 stdscr = curses.initscr()
-stdscr.clear()
+
+def isPlaying(self):
+        return pygame.mixer.music.get_busy()
+
+def Keyboard(Keyboard):
+        while True:
+                time.sleep(1)
+                
+t1 = threading.Thread(target=Keyboard, args=(1,))
+t1.start()
 
 while True:
         SongSelected = MusicFolder + "/" + random.choice(os.listdir(MusicFolder))
         tag = TinyTag.get(SongSelected)
-        pygame.mixer.init(tag.samplerate)
+        pygame.mixer.init(frequency=tag.samplerate)
         pygame.mixer.music.load(SongSelected)
         stdscr.addstr(SongSelected)
         pygame.mixer.music.play()
         while pygame.mixer.music.get_busy(): 
                 time.sleep(1)
                 stdscr.refresh()
+                ch = stdscr.getch()
+                if ch == ord('c'):
+                        if pause == 1:
+                            pygame.mixer.music.unpause()
+                            pause = 0
+                        else:
+                            pygame.mixer.music.pause()
+                            pause = 1
+                if ch == ord('x'):
+                    break
         stdscr.clear()
+        pygame.mixer.quit()
